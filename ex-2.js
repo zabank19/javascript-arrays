b1cf3d1a692cc1502a5f3556be56b5502d658e0b
Kant <98311953+Kladex@users.noreply.github.com>
2022-07-26 15:30:12 +0700

    Update ex-2.js

--- ex-2.js
+++ ex-2.js
@@ -1,14 +1,18 @@
-let employees = [
-  {
-    name: "James",
-    age: 40,
-    hobbies: ["Football", "Social media"],
-  },
-  {
-    name: "Alex",
-    age: 25,
-    hobbies: ["Cryptocurrency", "Social media"],
-  },
+// Exercise #2: Employees Array
+const employees = [
+	{
+		name: "James",
+		age: 40,
+		hobbies: ["Football", "Social media"]
+	},
+	{
+		name: "Alex",
+		age: 25,
+		hobbies: ["Cryptocurrency", "Social media"]
+	}
 ];
-
 // Start coding here
+let alex;
+let alexHobbies;
+let alexAndJamesAge;
+const updateEmployees = employees;
