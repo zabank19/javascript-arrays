af06f6894464409e7a6e501e16d3dda60eeb51b5
Kant <98311953+Kladex@users.noreply.github.com>
2022-07-26 15:35:58 +0700

    Update ex-2.js

--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,15 @@
 Exercise #2: Editing Employee Array
 const employees = [
-	{
-		name: "James",
-		age: 40,
-		hobbies: ["Football", "Social media"]
-	},
-	{
-		name: "Alex",
-		age: 25,
-		hobbies: ["Cryptocurrency", "Social media"]
-	}
+  {
+    name: "James",
+    age: 40,
+    hobbies: ["Football", "Social media"],
+  },
+  {
+    name: "Alex",
+    age: 25,
+    hobbies: ["Cryptocurrency", "Social media"],
+  },
 ];
 // Start coding here
 let alex;
